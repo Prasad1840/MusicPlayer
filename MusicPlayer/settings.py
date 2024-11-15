a39eb5e72138d244f60871d2f62e700fa1dc3904
Prasad1840 <114741322+Prasad1840@users.noreply.github.com>
2024-11-15 18:22:45 +0530

    Update settings.py

--- MusicPlayer/settings.py
+++ MusicPlayer/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'SECRET_KEY'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['musicplayer-qox4.onrender.com']
 
 
 # Application definition
